107e5b89f6e52fdb5102dc4874d4d0acb18a4dd9
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-03-31 20:49:10 +0200

    feat: create a snake class & move to OOP

--- main.py
+++ main.py
@@ -1,63 +1,19 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
-# x_position = [0, -20, -40]
-# y_positions = [0, 0, 0]
-# all_turtles = []
 
-# for turtle_index in range(0, 3):
-#     new_turtle = Turtle(shape="square")
-#     new_turtle.color("white")
-#     new_turtle.penup()
-#     new_turtle.goto(x=x_position[turtle_index], y=y_positions[turtle_index])
-#     all_turtles.append(new_turtle)
-
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
