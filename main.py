c1e66ce1bcefadf384e94e8d161f367d4ebef094
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-04-01 00:54:03 +0200

    feat: control the snake with a keypress

--- main.py
+++ main.py
@@ -10,10 +10,16 @@ screen.tracer(0)
 
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up,"w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.5)
     snake.move()
 
 screen.exitonclick()
