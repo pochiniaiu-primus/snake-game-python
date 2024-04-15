ea64401f320e89e32005a8b8a8b01a59793b3f8b
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-04-15 21:03:13 +0200

    feat: read and write the high score to a file

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard (Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_high_score()
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -22,6 +22,7 @@ class Scoreboard (Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_high_score()
         self.score = 0
         self.update_scoreboard()
 
@@ -32,3 +33,12 @@ class Scoreboard (Turtle):
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
+
+    def read_high_score(self):
+        with open("data.txt") as data:
+            contents = data.read()
+            return int(contents)
+
+    def write_high_score(self):
+        with open("data.txt", mode="w") as data:
+            data.write(f"{self.high_score}")
